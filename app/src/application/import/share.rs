1026b09bc29be3b50e0083164d81f77fa3109f97
Archer <3762193+archer-trek@users.noreply.github.com>
2026-04-10 19:47:27 +0800

    split share provider traversal

--- app/src/application/import/share.rs
+++ app/src/application/import/share.rs
@@ -1,3 +1,5 @@
+mod providers;
+
 use reqwest::Url;
 use tracing::info;
 
@@ -8,11 +10,6 @@ use crate::application::import_ports::{
 use crate::domain::import::{
     ShareUrl,
     inner::MediaFile,
-    share_collect::{
-        collect_pan115_directory_entries, collect_pan123_directory_entries,
-        collect_pan189_directory_entries,
-    },
-    share_walk::ShareTraversal,
     source::{parse_pan115_share_parts, parse_pan123_share_parts, parse_pan189_share_code},
 };
 use crate::error::{AppError, AppResult};
@@ -85,77 +82,5 @@ where
             provider
         );
         self.transfer_mut().transfer_media_files(&media_files).await
-    }
-
-    async fn list_files_from_pan123_share(
-        &mut self,
-        share_key: &str,
-        share_password: &str,
-    ) -> AppResult<Vec<MediaFile>> {
-        let mut traversal = ShareTraversal::new((0, String::new()));
-
-        while let Some((parent_id, parent_path)) = traversal.next_dir() {
-            let files = self
-                .share_source()
-                .list_pan123_share_files(share_key, share_password, parent_id)
-                .await?;
-
-            traversal.extend(collect_pan123_directory_entries(&files, &parent_path));
-        }
-
-        Ok(self
-            .metadata_lookup_mut()
-            .build_media_files(traversal.into_raw_files()))
-    }
-
-    async fn list_files_from_pan189_share(
-        &mut self,
-        share_code: &str,
-    ) -> AppResult<Vec<MediaFile>> {
-        let share_info = self
-            .share_source()
-            .get_pan189_share_info(share_code)
-            .await?;
-
-        let mut traversal =
-            ShareTraversal::new((share_info.file_id, share_info.file_name.to_owned()));
-
-        while let Some((parent_id, parent_path)) = traversal.next_dir() {
-            let (folders, files) = self
-                .share_source()
-                .list_pan189_share_files(share_info.share_id, share_info.share_mode, &parent_id)
-                .await?;
-
-            traversal.extend(collect_pan189_directory_entries(
-                &folders,
-                &files,
-                &parent_path,
-            ));
-        }
-
-        Ok(self
-            .metadata_lookup_mut()
-            .build_media_files(traversal.into_raw_files()))
-    }
-
-    async fn list_files_from_pan115_share(
-        &mut self,
-        share_code: &str,
-        receive_code: &str,
-    ) -> AppResult<Vec<MediaFile>> {
-        let mut traversal = ShareTraversal::new(("0".to_string(), String::new()));
-
-        while let Some((cid, parent_path)) = traversal.next_dir() {
-            let entries = self
-                .share_source()
-                .list_pan115_share_files(share_code, receive_code, &cid)
-                .await?;
-
-            traversal.extend(collect_pan115_directory_entries(&entries, &parent_path));
-        }
-
-        Ok(self
-            .metadata_lookup_mut()
-            .build_media_files(traversal.into_raw_files()))
     }
 }
