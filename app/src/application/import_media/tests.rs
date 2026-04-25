028e1307c919e4c14916b42a684ad3e04c918f56
Archer <3762193+archer-trek@users.noreply.github.com>
2026-04-25 16:21:13 +0800

    use pan189 cas names as context

--- app/src/application/import_media/tests.rs
+++ app/src/application/import_media/tests.rs
@@ -610,6 +610,59 @@ async fn import_from_pan189_share_imports_downloaded_cas_entries() {
 }
 
 #[tokio::test]
+async fn import_from_pan189_share_uses_cas_file_name_as_context_path() {
+    let local_dir = unique_temp_dir();
+    let share_source = FakeShareSource {
+        pan189_share_info: Arc::new(Mutex::new(Some(Pan189ShareInfo {
+            file_id: "root".into(),
+            file_name: "share-root".into(),
+            share_id: 1,
+            share_mode: 3,
+        }))),
+        pan189_files: Arc::new(Mutex::new(HashMap::from([(
+            "root".to_string(),
+            (
+                Vec::new(),
+                vec![Pan189File {
+                    id: "cas-1".into(),
+                    name: "Breaking Bad (2008) {tmdb-1396}.cas".into(),
+                    size: 288,
+                    md5: "79202e0c3975e92c12ee2b543ebcd968".into(),
+                }],
+            ),
+        )]))),
+        pan189_downloads: Arc::new(Mutex::new(HashMap::from([(
+            "cas-1".to_string(),
+            serde_json::json!({
+                "fileName": "S01E01.mp4",
+                "md5": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                "size": 1001
+            })
+            .to_string()
+            .into_bytes(),
+        )]))),
+        ..Default::default()
+    };
+    let service = ImportMediaService::new(
+        FakeLibraryGateway::default(),
+        share_source,
+        FakeMetadataCatalog,
+        local_store_for(&local_dir),
+    );
+    let url = Url::parse("https://cloud.189.cn/t/share189").unwrap();
+    let share = ShareUrl::from(&url).unwrap();
+
+    let imported = service.import_from_share_url(&share).await.unwrap();
+
+    assert!(matches!(
+        imported.as_slice(),
+        [ImportedMedia::Tv { name, season, episodes, total_size, has_failed, .. }]
+            if name == "Breaking Bad" && *season == 1 && episodes == &vec![1] && *total_size == 1001 && !has_failed
+    ));
+    let _ = fs::remove_dir_all(local_dir);
+}
+
+#[tokio::test]
 async fn import_from_share_url_walks_pan115_entries_and_imports_tv() {
     let local_dir = unique_temp_dir();
     let gateway = FakeLibraryGateway::default();
