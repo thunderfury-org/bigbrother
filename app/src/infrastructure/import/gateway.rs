688ddbfd4cf9db94ad69214392fe798f21a97482
Archer <3762193+archer-trek@users.noreply.github.com>
2026-05-14 20:15:34 +0800

    refactor: restore import port layering

--- app/src/infrastructure/import/gateway.rs
+++ app/src/infrastructure/import/gateway.rs
@@ -7,7 +7,7 @@ use crate::{
         Season, TvDetail,
     },
     application::{
-        import_ports::{LibraryGateway, MetadataCatalog},
+        import_ports::{LibraryGateway, MetadataCatalog, ShareSource},
         ports::{MediaDirectoryRecord, MediaSearchSource},
     },
     error::AppResult,
@@ -43,52 +43,6 @@ impl PanLibraryGateway {
     pub fn new(pan123: pan123::Client) -> Self {
         Self { pan123 }
     }
-}
-
-pub trait ShareSource: Clone {
-    async fn list_pan123_share_files(
-        &self,
-        share_key: &str,
-        share_password: &str,
-        parent_id: i64,
-    ) -> AppResult<Vec<LibraryFile>>;
-    async fn get_pan189_share_info(&self, share_code: &str) -> AppResult<Pan189ShareInfo>;
-    async fn list_pan189_share_files(
-        &self,
-        share_id: i64,
-        share_mode: i32,
-        parent_id: &str,
-    ) -> AppResult<(Vec<Pan189Folder>, Vec<Pan189File>)>;
-    async fn download_pan189_share_file(
-        &self,
-        share_id: i64,
-        file: &Pan189File,
-    ) -> AppResult<Vec<u8>>;
-    async fn list_pan115_share_files(
-        &self,
-        share_code: &str,
-        receive_code: &str,
-        cid: &str,
-    ) -> AppResult<Vec<Pan115FileEntry>>;
-    async fn get_quark_share_info(
-        &self,
-        share_id: &str,
-        password: &str,
-    ) -> AppResult<QuarkShareInfo>;
-    async fn list_quark_share_files(
-        &self,
-        share_id: &str,
-        password: &str,
-        stoken: &str,
-        pdir_fid: &str,
-    ) -> AppResult<(Vec<QuarkFolder>, Vec<QuarkFile>)>;
-    async fn batch_get_quark_file_md5s(
-        &self,
-        share_id: &str,
-        password: &str,
-        stoken: &str,
-        file_infos: &[(String, String)],
-    ) -> AppResult<HashMap<String, String>>;
 }
 
 impl ShareClientGateway {
